1ac4f845cc5607f3e4c02da62f9dd304a0263881
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2023-01-12 16:05:44 -0300

    feat: Structuring the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
+
 function App() {
   return (
     <div>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+      </ThemeProvider>
+
       <h1>Hello Github Blog</h1>
     </div>
   )
