f392709221bcaca8c1e6490356f09dcbf4589096
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2023-01-14 01:16:57 -0300

    feat: Rename DefaultLayout styles page

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
-import { LayoutContainer } from './styled'
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
